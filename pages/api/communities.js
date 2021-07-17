c5e33dc669ab314a1a3a1b8b4cb78394ca0994fb
Eduarda <eduardapinheiro1302@gmail.com>
2021-07-17 14:59:55 -0300

    feat: add datocms client

--- pages/api/communities.js
+++ pages/api/communities.js
@@ -1,5 +1,9 @@
+import { SiteClient } from 'datocms-client'
+
 export default async function requestReceiver(request, response) {
   const TOKEN = 'f57475ad409d1100dcacda411144ae'
+  const client = new SiteClient(TOKEN)
+
   console.lod(TOKEN)
   response.json({
     data: 'Algum dado qualquer'
