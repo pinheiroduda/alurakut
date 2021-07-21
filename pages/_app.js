af90dc9c58a955ae9b0dd4d4d6c7e20e60be5696
Eduarda <eduardapinheiro1302@gmail.com>
2021-07-20 21:48:19 -0300

    fix: background position

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,35 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import { AlurakutStyles } from '../src/lib/AlurakutCommuns'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons'
 
 const GlobalStyle = createGlobalStyle`
   /* Reset CSS */
   * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
-
   body {
     font-family: sans-serif;
     background-color: #FFB6C1;
     background-image: url('https://res-3.cloudinary.com/fieldfisher/image/upload/c_lfill,g_auto/f_auto,q_auto/v1/sectors/technology/tech_neoncircuitboard_857021704_medium_lc5h05');
     background-position: center;
   }
-
   #__next {
-    display: flex:
-    min-heigth: 100vh;
+    display: flex;
+    min-height: 100vh;
     flex-direction: column;
   }
-
   img {
     max-width: 100%;
     height: auto;
     display: block;
   }
-
   ${AlurakutStyles}
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3' // cor azul, pode ser mudada no projeto final
+    primary: '#0070f3'
   }
 }
 
